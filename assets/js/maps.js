0abc23e0bf94b87c8b93f41fd0709670faf28056
JonathanDelaney <glibbidydelaney@gmail.com>
2021-01-14 14:33:37 +0000

    buttons added to change steps on slider

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,37 +1,27 @@
-let locations = [];
+// document.write("<label id='year'>Tue Jul 28 1914 00:00:00 GMT-0025 (Irish Standard Time)</label><input id='slider' type='range' min='0' max='135397279000' step='86400000' value='0' onkeydown='return false;' /><br> <div id='map'>");
 
+function week() {
+    $("#slider").attr("step", "604800000")
+}
+
+function month() {
+    $("#slider").attr("step", "2628000000")
+}
+
+function threeMonths() {
+    $("#slider").attr("step", "7884000000")
+}
+
+let locations = [];
 var slider = document.getElementById("slider");
 var year = document.getElementById("year");
-var yearInt;
-let contempBattles = [];
-let contempCoords = [];
-var battles = [
-    {
-        battle: "battle of year 1914",
-        coords: { lat: 40.785091, lng: -73.968285 },
-        year: [1914, 1915, 1916],
-        description: "The bloody battle took place in 1903 mostly"
-    },
-    {
-        battle: "battle of year 1915",
-        coords: { lat: 41.084045, lng: -73.874245 },
-        year: [1915, 1916, 1917],
-        description: "The bloody battle took place in 1905 mostly"
-    },
-    {
-        battle: "battle of year 1918",
-        coords: { lat: 40.754932, lng: -73.984016 },
-        year: [1918],
-        description: "The bloody battle took place in 1972 mostly"
-    }
-]
 
-let tdate = [
+let wwOne = [
     {
         battle: "battle of year 1914",
         coords: { lat: 40.785091, lng: -73.968285 },
         startDate: "03/24/1914",
-        endDate: "03/20/1915",
+        endDate: "03/30/1916",
         description: "The bloody battle took place in 1903 mostly"
     },
     {
@@ -45,42 +35,31 @@ let tdate = [
         battle: "battle of year 1918",
         coords: { lat: 40.754932, lng: -73.984016 },
         startDate: "03/20/1918",
-        endDate: "03/24/1918",
+        endDate: "11/10/1918",
         description: "The bloody battle took place in 1972 mostly"
     }
-]
+];
 
 
-
-
-slider.addEventListener('mouseup', function (e) {
+slider.addEventListener('input', function (e) {
     let sliderDif = parseInt(slider.value);
     let dateShown = (1749252879000 - sliderDif) * (-1);
     console.log(dateShown);
     year.textContent = new Date(dateShown);
 
-    for (i = 0; i < tdate.length; i++) {
-        let startMsec = Date.parse(tdate[i].startDate);
-        let endMsec = Date.parse(tdate[i].endDate);
+    for (i = 0; i < wwOne.length; i++) {
+        let startMsec = Date.parse(wwOne[i].startDate);
+        let endMsec = Date.parse(wwOne[i].endDate);
 
         if (dateShown >= startMsec && dateShown <= endMsec) {
-            console.log(tdate[i].battle);
+            console.log(wwOne[i].battle);
+            locations.push(wwOne[i].coords)
         }
     }
-
-    contempBattles = battles.filter(o => o.year.includes(yearInt));
-    // console.log(contempBattles);
-    for (i = 0; i < contempBattles.length; i++) {
-        locations.push(contempBattles[i].coords)
-    }
-
-    // console.log(locations);
 
     initMap();
 
     locations = [];
-    contempCoords = [];
-    contempBattles = [];
 });
 
 function setMarkers() {
